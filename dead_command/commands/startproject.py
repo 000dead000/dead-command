0ecf3ddc42282708b0022c2acda9c44f5917524c
000PaRaDoX000 <ariel.calzada@gmail.com>
2017-04-18 07:26:45 -0500

    Fix Initial config transformation

--- dead_command/commands/startproject.py
+++ dead_command/commands/startproject.py
@@ -143,64 +143,64 @@ class StartprojectCommand(Common):
         """
         self.goto_directory(self.project_directory)
 
-        def adjust_settings(self):
-            conf_orig = os.path.join(
-                self.project_directory,
-                "conf.orig"
-            )
+    def register_settings(self):
+        conf_orig = os.path.join(
+            self.project_directory,
+            "conf.orig"
+        )
 
-            conf = os.path.join(
-                self.project_directory,
-                "conf"
-            )
+        conf = os.path.join(
+            self.project_directory,
+            "conf"
+        )
 
-            self.mv(conf_orig, conf)
-            self.remove_directory(conf_orig)
+        self.mv(conf_orig, conf)
+        self.remove_directory(conf_orig)
 
-            # add custom_settings call in settings.py
-            settings = os.path.join(
-                self.project_directory,
-                "conf",
-                "settings.py"
-            )
+        # add custom_settings call in settings.py
+        settings = os.path.join(
+            self.project_directory,
+            "conf",
+            "settings.py"
+        )
 
-            f = open(settings, "r")
-            contents = f.read()
-            f.close()
+        f = open(settings, "r")
+        contents = f.read()
+        f.close()
 
-            contents += """
+        contents += """
     try:
         execfile(os.path.join(BASE_DIR, 'conf', 'custom_settings.py'))
     except IOError:
         pass"""
 
-            f = open(settings, "w")
-            f.write(contents)
-            f.close()
+        f = open(settings, "w")
+        f.write(contents)
+        f.close()
 
-            """
-            settings = os.path.join(
-                self.project_directory,
-                "conf",
-                "custom_settings.py"
-            )
+        """
+        settings = os.path.join(
+            self.project_directory,
+            "conf",
+            "custom_settings.py"
+        )
 
-            f = open(settings, "r")
-            contents = f.read()
-            f.close()
+        f = open(settings, "r")
+        contents = f.read()
+        f.close()
 
-            contents = contents.replace("SLUG_PLACEHOLDER", self.slug)
-            contents = contents.replace("SHORT_TITLE_PLACEHOLDER", self.short_title)
-            contents = contents.replace("LONG_TITLE_PLACEHOLDER", self.long_title)
-            contents = contents.replace("DOMAIN_PLACEHOLDER", self.domain)
-            contents = contents.replace("EMAIL_USER_PLACEHOLDER", self.email)
-            contents = contents.replace("EMAIL_PASSWORD_PLACEHOLDER", self.password)
-            contents = contents.replace("EMAIL_BCC_RECIPIENT_PLACEHOLDER", self.email_bcc_recipient)
+        contents = contents.replace("SLUG_PLACEHOLDER", self.slug)
+        contents = contents.replace("SHORT_TITLE_PLACEHOLDER", self.short_title)
+        contents = contents.replace("LONG_TITLE_PLACEHOLDER", self.long_title)
+        contents = contents.replace("DOMAIN_PLACEHOLDER", self.domain)
+        contents = contents.replace("EMAIL_USER_PLACEHOLDER", self.email)
+        contents = contents.replace("EMAIL_PASSWORD_PLACEHOLDER", self.password)
+        contents = contents.replace("EMAIL_BCC_RECIPIENT_PLACEHOLDER", self.email_bcc_recipient)
 
-            f = open(settings, "w")
-            f.write(contents)
-            f.close()
-            """
+        f = open(settings, "w")
+        f.write(contents)
+        f.close()
+        """
 
     def export_skeleton(self):
         """ Export dar-skeleton to the project
