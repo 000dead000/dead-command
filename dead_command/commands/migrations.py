8889b03e9dce30fbb044c5b4ded9b22696cb51d1
000PaRaDoX000 <ariel.calzada@gmail.com>
2017-04-18 12:11:10 -0500

    pwd 2

--- dead_command/commands/migrations.py
+++ dead_command/commands/migrations.py
@@ -44,4 +44,4 @@ class MigrationsCommand(Common):
         # ]
         # self.run_command(command_arguments)
 
-        print "El hijo de florentina, ", os.pwd()
+        print "El hijo de florentina, ", os.curdir()
